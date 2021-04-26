10881036a6391442c374da60f6a18db0e6481469
mfauzanamzar <mfauzanamzar@gmail.com>
2021-04-26 10:33:28 +0800

    add logo

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
 import Navbar from '../components/Navbar'
+import styles from '../styles/Artikels.module.css'
 
 export default function Home() {
   
@@ -13,9 +13,15 @@ export default function Home() {
     
       </Head>
 
-      <main className={styles.main}>
+      <div className={styles.main}>
         <h1>Selamat Datang!</h1>
-      </main>
+      </div>
+      <div className={styles.main}>
+        <div className={styles.kotak}>Created by </div>
+        <h3>1. Jabalnur</h3>
+        <h3>2. Muhammad Fauzan Amzar</h3>
+        <h3>3. Muh. Ishak Ramadhan</h3>
+      </div>
 
  
     </div>
